63d8c1a5a0bc9baddedcd3fe5c0f14d5ed4d96ab
ABHIRAJ YADAV <102871390+abhiraj100@users.noreply.github.com>
2025-07-11 16:16:30 +0530

    Successfully added the functionality in the Contact us page

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React, { useState } from "react";
 
 const Contact = () => {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    // formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+    formData.append("access_key", "8f499b23-955d-4e24-b5d1-f19b955907c6");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
-    // <div id="contact" className='w-full px-[15%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")]'>
     <div
       id="contact"
       className="w-full px-[15%] py-10 scroll-mt-20 bg-cover bg-center bg-no-repeat "
@@ -17,6 +44,42 @@ const Contact = () => {
         I'd love to hear from you! If you have any questions, comments, or
         feedback, please use the form below.
       </p>
+
+      <form onSubmit={onSubmit} className="max-w-2xl mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 grid-cols-auto gap-6 mt-10 mb-8">
+          <input
+            type="text"
+            placeholder="Enter your name"
+            required
+            name="name"
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
+          />
+          <input
+            type="email"
+            placeholder="Enter your email"
+            required
+            name="email"
+            className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white"
+          />
+        </div>
+        <textarea
+          rows="6"
+          placeholder="Enter your message"
+          required
+          name="message"
+          className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6"
+        ></textarea>
+
+        <button
+          type="submit"
+          className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500"
+        >
+          Submit now{" "}
+          <Image src={assets.right_arrow_white} alt="" className="w-4" />
+        </button>
+
+        <p className="mt-4 text-black font-medium">{result}</p>
+      </form>
     </div>
   );
 };
